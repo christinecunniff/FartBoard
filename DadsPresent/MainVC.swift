ee45d1eca198de518e2c58f7cc1a0c4b5fdd0663
Christine Cunniff <christinecunniff@gmail.com>
2016-12-21 10:27:37 -0500

    Added MP3s to the project.

--- DadsPresent/MainVC.swift
+++ DadsPresent/MainVC.swift
@@ -7,19 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
